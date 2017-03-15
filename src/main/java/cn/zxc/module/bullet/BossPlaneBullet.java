aa9fe8efd6532b861f1436f8a7ee91462e43fb55
zhengxiaochuang <zhengxiaochuang@eefung.com>
2017-03-15 11:41:24 +0800

    血量

--- src/main/java/cn/zxc/module/bullet/BossPlaneBullet.java
+++ src/main/java/cn/zxc/module/bullet/BossPlaneBullet.java
@@ -7,7 +7,7 @@ package cn.zxc.module.bullet;
  *
  */
 public class BossPlaneBullet extends Bullet{
-	
+	private int bossHp;
 	@Override
 	public void move(int derection, int x, int y, int speed) {
 		
